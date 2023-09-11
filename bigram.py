73c5d5bbe9f67ba3ad5106f85b199988cc9d7061
Arthur <a.a.zwartsenberg@gmail.com>
2023-09-11 10:20:11 +0200

    in progress

--- bigram.py
+++ bigram.py
@@ -4,11 +4,12 @@ from torch.nn import functional as F
 
 block_size = 8
 batch_size = 32
-max_iters = 3000
+max_iters = 5000
 eval_interval = 300
-learning_rate = 0.01
+learning_rate = 1E-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
+n_embd = 32
 
 torch.manual_seed(1337)
 
@@ -49,30 +50,71 @@ def estimate_loss():
     model.train()
     return out
 
+class Head(nn.Module):
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias = False)
+        self.query = nn.Linear(n_embd, head_size, bias = False)
+        self.value = nn.Linear(n_embd, head_size, bias = False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        #Attention scores
+        wei = q @ k.transpose(-2, -1) * C**-0.5
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        wei = F.softmax(wei, dim = -1)
+        #Perform wegithed agg
+        v = self.value(x)
+        out = wei @ v
+        return out
+
+
 class BigramLanguageModel(nn.Module):
 
     def __init__(self, vocab_size):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        #Each token reads off logist for next token from lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.sa_head = Head(n_embd)
+        self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets = None):
-        logits = self.token_embedding_table(idx)
+        B, T = idx.shape
+        #Idx and target are both (B,T) tnsors of integers
+        tok_emb = self.token_embedding_table(idx)
+        pos_emb = self.position_embedding_table(torch.arange(T, device = device))
+        x = tok_emb + pos_emb #(B, T,C)
+        x = self.sa_head(x) #Apply one head of self-attention (B,T,C)
+        logits = self.lm_head(x) #B, T vocab size
         if targets is None:
             loss = None
         else:
                 
             B, T, C = logits.shape
             logits = logits.view(B*T, C)
-            targets = targets.view(-1)
+            targets = targets.view(B*T)
             loss = F.cross_entropy(logits, targets)
         return logits, loss
     
     def generate(self, idx, max_new_tokens):
+        #idx is (B, T) array of indices in current cont
         for _ in range(max_new_tokens):
-            logits, loss = self(idx)
+            #crop idx to last block_size tokens
+            idx_cond = idx[:, -block_size:]
+            #Get pred
+            logits, loss = self(idx_cond)
+            #Focus on last time step
             logits = logits[:, -1, :]
+            # Apply softmax to get probs
             probs = F.softmax(logits, dim =-1)
+            # Sample from dist
             idx_next = torch.multinomial(probs, num_samples = 1)
+            # Append sampled index
             idx = torch.cat((idx, idx_next), dim = 1)
         return idx
     xb, yb = get_batch('train')
@@ -95,4 +137,4 @@ for iter in range(max_iters):
     optimizer.step()
 
 context = torch.zeros((1, 1), dtype = torch.long, device = device)
-print(decode(m.gernerate(context, max_new_tokens=500)[0].tolist()))
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
